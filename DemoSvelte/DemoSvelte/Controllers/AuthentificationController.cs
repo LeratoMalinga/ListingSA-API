c61f29819344c1b915f5f1db87798e5982476be8
Lerato Malinga <lmalinga@retrorabbit.co.za>
2023-01-28 19:14:47 +0200

    Made changes

--- DemoSvelte/DemoSvelte/Controllers/AuthentificationController.cs
+++ DemoSvelte/DemoSvelte/Controllers/AuthentificationController.cs
@@ -15,13 +15,13 @@ namespace DemoSvelte.Controllers
             this.appUserService = appUserService;
         }
 
-        [HttpGet("GetAppUsers")]
-        public ActionResult<List<AppUser>> GetAppUsers()
-        {
-            return appUserService.Get();
-        }
+        //[HttpGet("GetAppUsers")]
+        //public ActionResult<List<AppUser>> GetAppUsers()
+        //{
+        //    return appUserService.Get();
+        //}
 
-        [HttpGet("Get UserAppUserbyId/{id}")]
+        [HttpGet("GetUserAppUserbyId/{id}")]
         public ActionResult<AppUser> GetAppUserbyId(string id)
         {
             var appUser = appUserService.Get(id);
@@ -34,44 +34,44 @@ namespace DemoSvelte.Controllers
             return appUser;
         }
 
-        [HttpPost("Add AppUser")]
-        public ActionResult<AppUser> Post([FromBody] AppUser appUser)
-        {
-            appUserService.Create(appUser);
+        //[HttpPost("RegisterAppUser")]
+        //public ActionResult<AppUser> Post([FromBody] AppUser appUser)
+        //{
+        //    appUserService.Create(appUser);
 
-            return CreatedAtAction(nameof(GetAppUsers), new { id = appUser.Id }, appUser);
-        }
+        //    return CreatedAtAction(nameof(GetAppUsers), new { id = appUser.Id }, appUser);
+        //}
 
-        // PUT api/<StudentsController>/5
-        [HttpPut("{id}")]
-        public ActionResult Put(string id, [FromBody] AppUser appUser)
-        {
-            var existingProperty = appUserService.Get(id);
+        
+        //[HttpPut("UpdateAppUser/{id}")]
+        //public ActionResult Put(string id, [FromBody] AppUser appUser)
+        //{
+        //    var existingProperty = appUserService.Get(id);
 
-            if (existingProperty == null)
-            {
-                return NotFound($"AppUser with Id = {id} not found");
-            }
+        //    if (existingProperty == null)
+        //    {
+        //        return NotFound($"AppUser with Id = {id} not found");
+        //    }
 
-            appUserService.Update(id, appUser);
+        //    appUserService.Update(id, appUser);
 
-            return NoContent();
-        }
+        //    return NoContent();
+        //}
 
-        // DELETE api/<StudentsController>/5
-        [HttpDelete("Delete AppUser/{id}")]
-        public ActionResult DeleteAppUser(string id)
-        {
-            var appUser = appUserService.Get(id);
+        
+        //[HttpDelete("Delete AppUser/{id}")]
+        //public ActionResult DeleteAppUser(string id)
+        //{
+        //    var appUser = appUserService.Get(id);
 
-            if (appUser == null)
-            {
-                return NotFound($"AppUser with Id = {id} not found");
-            }
+        //    if (appUser == null)
+        //    {
+        //        return NotFound($"AppUser with Id = {id} not found");
+        //    }
 
-            appUserService.Delete(appUser.Id);
+        //    appUserService.Delete(appUser.Id);
 
-            return Ok($"AppUser with Id = {id} deleted");
-        }
+        //    return Ok($"AppUser with Id = {id} deleted");
+        //}
     }
 }
