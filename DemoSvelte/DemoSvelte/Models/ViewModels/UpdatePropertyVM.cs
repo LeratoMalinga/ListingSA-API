cfc7a1533dd81e6bf005e5d02d57765293f2d51f
Lerato Malinga <lmalinga@retrorabbit.co.za>
2023-04-21 15:09:12 +0200

    Updates

---
+++ DemoSvelte/DemoSvelte/Models/ViewModels/UpdatePropertyVM.cs
@@ -0,0 +1,26 @@
+﻿namespace DemoSvelte.Models.ViewModels
+{
+    public class UpdatePropertyVM
+    {
+        public string Id { set; get; } = String.Empty;
+
+        public string Name { set; get; } = String.Empty;
+
+        public string Description { set; get; } = String.Empty;
+
+        public string Province { set; get; } = String.Empty;
+
+        public string City { set; get; } = String.Empty;
+
+        public string Suburb { set; get; } = String.Empty;
+
+        public string Price { set; get; } = String.Empty;
+
+        public string Address { set; get; } = String.Empty;
+
+        public string ImageBase64 { set; get; } = String.Empty;
+        public string Type { set; get; } = String.Empty;
+
+        public string UserId { set; get; } = String.Empty;
+    }
+}
